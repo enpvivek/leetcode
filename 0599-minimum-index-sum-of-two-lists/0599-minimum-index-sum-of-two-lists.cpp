6ef7b9c05da6cecd2ca75c214cde8fdaa8ea28dd
Vivek Yadav <90546533+enpvivek@users.noreply.github.com>
2023-02-12 22:33:25 +0530

    Time: 200 ms (35.54%), Space: 30.3 MB (85.80%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
-        int minIdx = 2000;
-        unordered_map<string, int> mp;
-        for(int i = 0; i < list1.size(); i++){
-            for(int j = 0; j < list2.size(); j++){
-                if(list1[i]==list2[j]){
-                    if(i+j<=minIdx){
-                        mp[list1[i]]=i+j;
+    vector<string> findRestaurant(vector<string>& a, vector<string>& b) {
+        vector<string> c;
+            int min = INT_MAX;
+            for (int i = 0; i < a.size(); i++)
+            {
+                for (int j = 0; j < b.size(); j++)
+                {
+                    if (a[i] == b[j])
+                    {
+                        if (i + j < min)
+                        {
+                            min = i + j;
+                            c.clear();
+                            c.push_back(a[i]);
+                        }
+                        else if (i + j == min)
+                        {
+                            c.push_back(a[i]);
+                        }
                     }
-                    minIdx = min(minIdx, i+j);
                 }
             }
-        }
-        vector<string>ans;
-        for(auto it: mp){
-            if(it.second==minIdx){
-                ans.push_back(it.first);
-            }
-        }
-        return ans;
+            return c;
     }
 };
